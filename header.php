2348ccb92aaa03ca38d1b011e45c17701cb1113b
Yi Chen <yichen@gmail.com>
2017-12-08 07:11:50 -0800

    initial commit

---
+++ header.php
@@ -0,0 +1,5 @@
+<div id="header"> 
+  <div id="logo"><a href="http://www.yichenphoto.com"><img src="/images/logo.jpg" width="190" height="35" border="0" alt="Seattle fine art Photographer"  /></a></div>
+  <ul id="nav">
+  </ul>
+</div>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<META NAME="DESCRIPTION" CONTENT="Seattle Fine Art Photographer">
+<META NAME="KEYWORDS" CONTENT="seattle fine art photographer, portrait">
+<title>Yi Chen Photography</title>
+<link href="style/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<?php include("header.php"); ?>
+<div id="content">
+  <div style="width:100%;text-align:center;padding:30px 0;">
+  <a href="/friends/" title="Friends"><img src="/friends/images/DSC_2977.jpg" width="150" border="0" style="margin:20px;" /></a>
+  <a href="/salsa/" title="Salsa"> <img src="/salsa/images/DSC_4185.jpg" width="150" border="0" style="margin:20px;" /></a>
+ <a href="/rafting/" title="Rafting the Yak"> <img src="/rafting/images/DSC_9274.jpg" width="150" border="0" style="margin:20px;" /></a>
+<br/>
+ <a href="/lake22/" title="Lake 22"> <img src="/lake22/images/DSC_8965.jpg" width="150" border="0" style="margin:20px;" /></a>
+ <a href="/london/" title="London"> <img src="/london/images/DSC_6168.jpg" width="150" border="0" style="margin:20px;" /></a>
+
+
+  </div>
+</div>
+<div id="footer"><strong>Email</strong>: yichen @ yiphoto.com 
+  </div>
+  
+</body>
+</html>
